a3dba0a3fb5ea55f4271b75fd4456ea723644752
Bashayr Alhasani <bashaier.alhassani@gmail.com>
2022-03-24 09:16:54 +0300

    changing the db-url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
   
 module.exports = {
-  url: process.env.DB
+  url: "mongodb://localhost:27017/ideas-lab"
+  // url:  "mongodb://mongodb-service:27017/ideas-lab"
 }; 
 
 
