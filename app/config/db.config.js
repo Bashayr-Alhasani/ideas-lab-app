3212f85486c6c1e9c706c520ac3101bd042379c0
Bashayr Alhasani <bashaier.alhassani@gmail.com>
2022-03-24 09:25:43 +0300

    Adding ejs/views

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
   
 module.exports = {
-  url: "mongodb://localhost:27017/ideas-lab"
-  // url:  "mongodb://mongodb-service:27017/ideas-lab"
+  // url: "mongodb://localhost:27017/ideas-lab"
+  url:  "mongodb://mongodb-service:27017/ideas-lab"
 }; 
 
 
